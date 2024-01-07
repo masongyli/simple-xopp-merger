0d03b1310b2196132d993ccf1f018e22c846e7f2
masongyli <masongyli@gmail.com>
2024-01-07 20:54:10 +0800

    Add script to merge xopp files into a single xopp file

---
+++ merge_xopp.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+tmp_folder=".xopp_merge_tmp"
+mkdir $tmp_folder
+
+# Create a xml.gz replica for each .xopp file of the current folder
+for file in *.xopp; do
+    if [ -e "$file" ]; then
+        # Rename the file to have .gz extension
+        cp "$file" "$tmp_folder/${file%.xopp}.xml.gz"
+        echo "Copied $file to $tmp_folder/${file%.xopp}.xml.gz"
+    fi
+done
+
+# Decompress all .gz files
+for file in $tmp_folder/*.gz; do
+    if [ -e "$file" ]; then
+        gzip -d "$file"
+        echo "Decompressed $file"
+    fi
+done
+
+output_file="merged_xopp.xml"
+
+# Create a new xournal file with the common head
+echo '<?xml version="1.0" standalone="no"?>' >> "$output_file"
+echo '<xournal creator="Xournal++ 1.2.2" fileversion="4">' >> "$output_file"
+echo '<title>Xournal++ document - see https://github.com/xournalpp/xournalpp</title>' >> "$output_file"
+echo '<preview></preview>' >> "$output_file"
+# content in the <preview> tag is the base64 image encoding of the first page of the document
+# Whenever we execute "save file" in Xournal++, the content of preview is updated
+# so we don't need to worry about it
+
+# Iterate over each .xml file
+for file in $tmp_folder/*.xml; do
+    if [ -e "$file" ]; then
+        # Extract the content between the first <page> and the last </page>
+        echo '<!-- content from $file -->' >> "$output_file" 
+        sed -n '/<page/,/<\/page>/p' "$file" >> "$output_file"
+        echo "Appended the page content of $file to $output_file"
+    fi
+done
+
+echo '</xournal>' >> "$output_file"
+
+
+gzip $output_file 
+mv "$output_file.gz" "${output_file%.xml}.xopp"
+echo "Generated ${output_file%.xml}.xopp"
+
+# remove tmp files
+rm -r $tmp_folder
+echo "Removed $tmp_folder"
+
+echo "Done"
